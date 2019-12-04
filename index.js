7bb643db4dae0757ea69036e81e45ada8907fef1
Victoria Sanko <vsanko@Landsort.local>
2019-12-04 23:30:34 +0300

    feat: make server with map, and userId

--- index.js
+++ index.js
@@ -1,27 +1,71 @@
+const session = require('express-session');
 const express = require("express");
 const http = require('http');
 const WebSocket = require("ws");
+const uuid = require('uuid');
+
 const app = express();
 
-//initialize a simple http server
-const server = http.createServer(app);
 
-//initialize the WebSocket server instance
-const wss = new WebSocket.Server({ server });
+const map = new Map();
 
-wss.on('connection', (ws) => {
-
-    //connection is up, let's add a simple simple event
-    ws.on('message', (message) => {
-
-        //log the received message and send it back to the client
-        console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
-    });
-
-    //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server');
+const sessionParser = session({
+    saveUninitialized: false,
+    secret: '$eCuRiTy',
+    resave: false
 });
+
+
+const server = http.createServer(app);
+const wsServer = new WebSocket.Server({ clientTracking: false, noServer: true });
+
+server.on('upgrade', function(request, socket, head) {
+  console.log('Parsing session from request...');
+
+  sessionParser(request, {}, () => {
+    // if (!request.session.userId) {
+    //   socket.destroy();
+    //   console.log('!request.session.userId', !request.session.userId);
+      
+    //   return;
+    // }
+
+
+    wsServer.handleUpgrade(request, socket, head, function(ws) {
+        const id = uuid.v4();
+        request.session.userId = id;
+        wsServer.emit('connection', ws, request);
+    });
+  });
+});
+
+
+wsServer.on('connection', (ws, request) => {
+    const userId = request.session.userId;
+
+    map.set(userId, ws);
+
+  console.log('map', map);
+  
+    ws.on('message', function(message) {
+        console.log(`Received message ${message} from user ${userId}`);
+        map.forEach((value, key, map)=> {
+            console.log('key', key);
+            console.log('map', map);
+            
+            if(key!==userId){
+                 ws.send(`Hello, you sent -> ${message}`);
+                }
+            }
+        )
+    });
+  
+    ws.on('close', function() {
+      map.delete(userId);
+    });
+  });
+  
+
 
 //start our server
 server.listen(process.env.PORT || 8080, () => {
